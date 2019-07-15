7a2e7754d1fabc9529540b1c03dc79df2c2d48cf
Hyunjooleeee <hjdjl7@naver.com>
2019-07-15 16:06:33 +0900

    second commit

--- app.js
+++ app.js
@@ -7,13 +7,29 @@ const port = 3000
 
 const path = require('path');
 const router = express.Router();
-router.get('/', function (req, res) {
-res.sendFile(path.join(__dirname + '/index.html'));
+
+router.get('/sub01', function (req, res) {
+    res.sendFile(path.join(__dirname + '/sub_stylesheets/sub01.html'));
+    //__dirname : It will resolve to your project folder.
+    });
+
+router.get('/sub02', function (req, res) {
+res.sendFile(path.join(__dirname + '/sub_stylesheets/sub02.html'));
 //__dirname : It will resolve to your project folder.
 });
+
+router.get('/sub03', function (req, res) {
+res.sendFile(path.join(__dirname + '/sub_stylesheets/sub03.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/sub04', function (req, res) {
+    res.sendFile(path.join(__dirname + '/sub_stylesheets/sub04.html'));
+    //__dirname : It will resolve to your project folder.
+    });
 
 app.use('/', router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
